e59806a59f47cbfb01642bd9153eea9a513c8add
Maksym Yatsenko <maksym.yatsenko@student.tuke.sk>
2024-02-25 20:38:27 +0100

    ps1

--- ps1/bmp.c
+++ ps1/bmp.c
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include "bmp.h"
 
-char* unique_reverse(const char*text){
+char* reverse(const char*text){
     if (text==NULL)
         return NULL;
     int lenght = strlen(text);
@@ -17,14 +17,76 @@ char* unique_reverse(const char*text){
     reversed[lenght]='\0';
     return reversed;
 }
-int main(){
+
+
+char* vigenere_encrypt(const char* key, const char* text) {
+    if (key == NULL || text == NULL) {
+        return NULL;
+    }
+
+    int key_length = strlen(key);
+    int text_length = strlen(text);
+    char* encrypted_text = (char*)malloc((text_length + 1) * sizeof(char));
+    if (encrypted_text == NULL) {
+        return NULL;
+    }
+    for (int i = 0, j = 0; i < text_length; i++) {
+        char current_char = text[i];
+        char key_char = toupper(key[j % key_length]);
+        char encrypted_char;
+
+        if (isalpha(current_char)) {
+            char base = isupper(current_char) ? 'A' : 'a';
+            encrypted_char = base + (current_char - base + key_char - 'A') % 26;
+            j++;
+        } else {
+            encrypted_char = current_char;
+        }
+
+        encrypted_text[i] = encrypted_char;
+    }
+    encrypted_text[text_length] = '\0';
+
+    return encrypted_text;
+}
+char* vigenere_decrypt(const char* key, const char* text) {
+    if (key == NULL || text == NULL) {
+        return NULL;
+    }
+
+    int key_length = strlen(key);
+    int text_length = strlen(text);
+    char* decrypted_text = (char*)malloc((text_length + 1) * sizeof(char));
+    if (decrypted_text == NULL) {
+        return NULL;
+    }
+    for (int i = 0, j = 0; i < text_length; i++) {
+        char current_char = text[i];
+        char key_char = toupper(key[j % key_length]);
+        char decrypted_char;
+
+        if (isalpha(current_char)) {
+            char base = isupper(current_char) ? 'A' : 'a';
+            decrypted_char = base + (current_char - key_char + 26) % 26;
+            j++;
+        } else {
+            decrypted_char = current_char;
+        }
+
+        decrypted_text[i] = decrypted_char;
+    }
+    decrypted_text[text_length] = '\0';
+
+    return decrypted_text;
+}
+
+int main() {
     const char* input = "Hello world!";
-    char* reversed = unique_reverse(input);
-    if(reversed !=NULL){
+    char* reversed = reverse(input);
+    if (reversed != NULL) {
         printf("%s\n", reversed);
         free(reversed);
-    }
-    else{
+    } else {
         printf("Input is NULL.\n");
     }
     return 0;
